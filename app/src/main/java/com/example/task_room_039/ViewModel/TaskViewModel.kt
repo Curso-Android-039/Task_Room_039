a0a018b3ed004c866c80dfc6239c96bca1ea304a
HomeStudio <Mario_cane.do@hotmail.com>
2024-10-15 20:52:59 -0300

    se implementa ViewModel , adapter y recyclerView Falta implementaciòn de segundo fragmento. con Button guardar.

--- app/src/main/java/com/example/task_room_039/ViewModel/TaskViewModel.kt
+++ app/src/main/java/com/example/task_room_039/ViewModel/TaskViewModel.kt
@@ -1,8 +1,10 @@
 package com.example.task_room_039.ViewModel
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.task_room_039.Modelo.Task
 import com.example.task_room_039.Modelo.TaskDatabase
@@ -25,11 +27,38 @@ class TaskViewModel(application : Application) :AndroidViewModel(application){
     }
 
 
-    // INSERT
+    // INSERT corrutinas se esta ejecutando en un hilo secundario
     fun insertTask(task: Task) = viewModelScope.launch {
 
         repository.insertTask(task)
     }
+
+
+    fun deleteTask(task: Task) = viewModelScope.launch {
+        repository.deleteTask(task)
+    }
+
+    // eliminar todo
+    fun deleteAllTask() = viewModelScope.launch {
+        repository.deleteAllTask()
+    }
+
+
+    // recibe el objeto seleccionado lo envuelve en liveDATA para poder observarlo
+
+    private var selectedTask : MutableLiveData<Task> = MutableLiveData()
+
+
+    // va a servir cuando selecciono una tarea y la envia
+    fun selected(task: Task){
+        selectedTask.value = task
+
+    }
+
+    // para recibir el objeto seleccionado
+    fun selectedItem(): LiveData<Task> = selectedTask
+
+
 
 
 
